f107271feb0ee69cc4150fb1d32ac6568d6d2bd0
Hugo Adriano de Oliveira <hugoadrianodeoliveira@gmail.com>
2023-05-10 22:21:37 -0400

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿
+namespace SistemDeAprovaçãodeAlunos
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtNotaFinal = new System.Windows.Forms.TextBox();
+            this.txtIdade = new System.Windows.Forms.TextBox();
+            this.mskDataNascimento = new System.Windows.Forms.MaskedTextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dgvAlunos = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNotaInicial = new System.Windows.Forms.TextBox();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtNotaFinal);
+            this.panel2.Controls.Add(this.txtIdade);
+            this.panel2.Controls.Add(this.mskDataNascimento);
+            this.panel2.Controls.Add(this.txtNome);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(249, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(509, 160);
+            this.panel2.TabIndex = 1;
+            // 
+            // txtNotaFinal
+            // 
+            this.txtNotaFinal.Location = new System.Drawing.Point(437, 80);
+            this.txtNotaFinal.Name = "txtNotaFinal";
+            this.txtNotaFinal.Size = new System.Drawing.Size(55, 25);
+            this.txtNotaFinal.TabIndex = 10;
+            // 
+            // txtIdade
+            // 
+            this.txtIdade.Enabled = false;
+            this.txtIdade.Location = new System.Drawing.Point(279, 80);
+            this.txtIdade.Name = "txtIdade";
+            this.txtIdade.Size = new System.Drawing.Size(65, 25);
+            this.txtIdade.TabIndex = 9;
+            // 
+            // mskDataNascimento
+            // 
+            this.mskDataNascimento.Location = new System.Drawing.Point(134, 80);
+            this.mskDataNascimento.Mask = "00/00/0000";
+            this.mskDataNascimento.Name = "mskDataNascimento";
+            this.mskDataNascimento.Size = new System.Drawing.Size(89, 25);
+            this.mskDataNascimento.TabIndex = 8;
+            this.mskDataNascimento.ValidatingType = typeof(System.DateTime);
+            this.mskDataNascimento.MaskInputRejected += new System.Windows.Forms.MaskInputRejectedEventHandler(this.maskedTextBox1_MaskInputRejected);
+            this.mskDataNascimento.Leave += new System.EventHandler(this.maskedTextBox1_Leave);
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(126, 41);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(355, 25);
+            this.txtNome.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(361, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(70, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Nota Final:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(229, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Idade:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(16, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Data Nascimento:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 44);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Nome do Aluno:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Dados do Aluno";
+            // 
+            // dgvAlunos
+            // 
+            this.dgvAlunos.AllowUserToAddRows = false;
+            this.dgvAlunos.AllowUserToDeleteRows = false;
+            this.dgvAlunos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAlunos.Location = new System.Drawing.Point(12, 237);
+            this.dgvAlunos.Name = "dgvAlunos";
+            this.dgvAlunos.ReadOnly = true;
+            this.dgvAlunos.RowTemplate.Height = 27;
+            this.dgvAlunos.Size = new System.Drawing.Size(746, 150);
+            this.dgvAlunos.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Incluir Aluno";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 188);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(145, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Reavaliar Alunos";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 214);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Aluno";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNotaInicial);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 159);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Média Geral";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // txtNotaInicial
+            // 
+            this.txtNotaInicial.Location = new System.Drawing.Point(6, 43);
+            this.txtNotaInicial.Name = "txtNotaInicial";
+            this.txtNotaInicial.Size = new System.Drawing.Size(188, 25);
+            this.txtNotaInicial.TabIndex = 11;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "Nome do Aluno";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dgvAlunos);
+            this.Controls.Add(this.panel2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAlunos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dgvAlunos;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtNotaFinal;
+        private System.Windows.Forms.TextBox txtIdade;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNotaInicial;
+        private System.Windows.Forms.MaskedTextBox mskDataNascimento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SistemDeAprovaçãodeAlunos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            dgvAlunos.ColumnCount = 5;
+            dgvAlunos.Columns[0].Name = "Nome";
+            dgvAlunos.Columns[1].Name = "Data de Nascimento";
+            dgvAlunos.Columns[2].Name = "Idade";
+            dgvAlunos.Columns[3].Name = "Nota Final";
+            dgvAlunos.Columns[4].Name = "Situação";
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nome = txtNome.Text;
+            DateTime dataNascimento = DateTime.Parse(mskDataNascimento.Text);
+            int idade = DateTime.Today.Year - dataNascimento.Year;
+
+            double notaFinal = double.Parse(txtNotaFinal.Text);
+            if (string.IsNullOrEmpty(txtNome.Text) || string.IsNullOrEmpty(mskDataNascimento.Text) || string.IsNullOrEmpty(txtIdade.Text) || string.IsNullOrEmpty(txtNotaFinal.Text))
+            {
+                MessageBox.Show("Preencha todos os campos antes de adicionar o aluno.", "Campos em branco", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            double média = double.Parse(txtNotaInicial.Text);
+            if (notaFinal >= média)
+            {
+                dgvAlunos.Rows.Add(nome, dataNascimento.ToShortDateString(), idade, notaFinal, "Aprovado");
+                MessageBox.Show("Aluno adicionado com sucesso!", "Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            }
+            else
+            {
+                dgvAlunos.Rows.Add(nome, dataNascimento.ToShortDateString(), idade, notaFinal, "Reprovado");
+
+            }
+            LimpaCampos();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int index = dgvAlunos.SelectedCells[0].RowIndex;
+            double notaFinal = double.Parse(txtNotaFinal.Text);
+            double média = double.Parse(txtNotaInicial.Text);
+
+            if (notaFinal >= média)
+            {
+                dgvAlunos.Rows[index].Cells[3].Value = notaFinal;
+                dgvAlunos.Rows[index].Cells[4].Value = "Aprovado";
+            }
+            else
+            {
+                dgvAlunos.Rows[index].Cells[3].Value = notaFinal;
+                dgvAlunos.Rows[index].Cells[4].Value = "Reprovado";
+            }
+        }
+        private void LimpaCampos()
+        {
+            txtNome.Clear();
+            txtIdade.Clear();
+            txtNotaFinal.Clear();
+            mskDataNascimento.Clear();
+            txtNome.Focus();
+        }
+        private void maskedTextBox1_Leave(object sender, EventArgs e)
+        {
+            DateTime dataNascimento;
+            if (DateTime.TryParse(mskDataNascimento.Text, out dataNascimento))
+            {
+                int idade = DateTime.Today.Year - dataNascimento.Year;
+                if (DateTime.Today < dataNascimento.AddYears(idade))
+                {
+                    idade--;
+                }
+                
+                txtIdade.Text = idade.ToString();
+
+            }
+            else
+            {
+                MessageBox.Show("Digite uma data valida");
+                mskDataNascimento.Clear();
+                mskDataNascimento.Focus();
+                return;
+            }
+
+
+        }
+        
+    }
+}
